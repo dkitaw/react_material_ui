2a8f432dfac6bdbd1e43ce593150a47e716bb499
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-12-19 21:50:39 -0500

    Typograph added to header component

--- 01_demo_one/src/components/Layouts/Header.js
+++ 01_demo_one/src/components/Layouts/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core'
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 export default props => {
   return (
-    <AppBar >
-        <Toolbar>
-          
-        </Toolbar>
-      </AppBar>
+    <AppBar>
+      <Toolbar>
+        <Typography variant="headline" color="inherit">
+          Exercise Database
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
