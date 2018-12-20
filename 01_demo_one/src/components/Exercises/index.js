7c0c25ff569c03ece10fc8f398beb9a06db0b015
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-12-20 01:52:22 -0500

    laft lane filled with data

--- 01_demo_one/src/components/Exercises/index.js
+++ 01_demo_one/src/components/Exercises/index.js
@@ -1,20 +1,57 @@
-import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
-import LeftPane from './LeftPane';
-import RightPane from './RightPane';
+import React, { Fragment } from 'react';
+import {
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemLink
+} from '@material-ui/core';
+
 const styles = {
-  Paper: {padding: 20, marginTop: 10, marginBottom: 10}
-}
-export default props => {
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 500,
+    overflowY: 'auto'
+  }
+};
+export default ({ exercises }) => {
   return (
     <Grid container>
       <Grid item sm>
-        <LeftPane styles={styles}/>
+        <Paper style={styles.Paper}>
+          {exercises.map(([group, exercises]) => (
+            <Fragment>
+              <Typography
+                variant="headline"
+                style={{ textTransform: 'capitalize' }}
+              >
+                {group}
+              </Typography>
+              <List component="ul">
+                {exercises.map(({ title }) => {
+                  return (
+                    <ListItem button>
+                      <ListItemText primary={title} />
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </Fragment>
+          ))}
+        </Paper>
       </Grid>
       <Grid item sm>
-        <RightPane styles={styles}/>
+        <Paper style={styles.Paper}>
+          <Typography variant="display1">Welcome!</Typography>
+          <Typography variant="Subheading" style={{ marginTop: 20 }}>
+            Pleas select an exercise from the list on the left
+          </Typography>
+        </Paper>
       </Grid>
-     
     </Grid>
   );
 };
--- 01_demo_one/src/components/Layouts/Footer.js
+++ 01_demo_one/src/components/Layouts/Footer.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import {Paper, Tabs, Tab} from '@material-ui/core';
-export default props => {
+import { Paper, Tabs, Tab } from '@material-ui/core';
+export default ({ muscles }) => {
   return (
     <Paper>
-      <Tabs
-        value={0}
-        indicatorColor="primary"
-        textColor="primary"
-        centered
-      >
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
+      <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
+        <Tab label="All" />
+        {muscles.map(muscle => (
+          <Tab label={muscle} />
+        ))}
       </Tabs>
     </Paper>
   );
