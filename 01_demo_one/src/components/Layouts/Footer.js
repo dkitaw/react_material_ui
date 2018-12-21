2fef6fe05a9b5e9c5fd57d2aef7e928e156e3969
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-12-20 22:12:33 -0500

    footer tab selection defined

--- 01_demo_one/src/components/Layouts/Footer.js
+++ 01_demo_one/src/components/Layouts/Footer.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { Paper, Tabs, Tab } from '@material-ui/core';
 export default ({ muscles, category, onSelect }) => {
   console.log(category);
-  const index = category ? muscles.findIndex(group => group === category)+1 : 0;
+  const index = category
+    ? muscles.findIndex(group => group === category) + 1
+    : 0;
+  const onIndexSelect = (e, index) => {
+    onSelect(index === 0 ? '' : muscles[index - 1]);
+  };
   return (
     <Paper>
-      <Tabs value={index} indicatorColor="primary" textColor="primary" centered>
+      <Tabs
+        value={index}
+        onChange={onIndexSelect}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
         <Tab label="All" />
         {muscles.map(muscle => (
           <Tab label={muscle} />
